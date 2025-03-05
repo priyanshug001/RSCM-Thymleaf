5acc7ab4e8fba87605fcee60973eb7f8378cdc48
priyanshug001 <154699980+priyanshug001@users.noreply.github.com>
2025-03-05 12:18:24 +0530

    Update MainController.java

--- src/main/java/com/RSCM/Controller/MainController.java
+++ src/main/java/com/RSCM/Controller/MainController.java
@@ -124,12 +124,18 @@ public class MainController {
 
 	// Index Page Content and Reference
 
-	@GetMapping({"/", "/index"})
+	@GetMapping("/")
 	public String HomePage(Model model) {
 		returnIndexPageData(model);
 		returnUpdatedServicesData(model);
 		returnUpdatedAboutPageData(model);
 		return "index";
 	}
-
+       @GetMapping("/index")
+	public String HomePage(Model model) {
+		returnIndexPageData(model);
+		returnUpdatedServicesData(model);
+		returnUpdatedAboutPageData(model);
+		return "redirect:/";
+	}
 }

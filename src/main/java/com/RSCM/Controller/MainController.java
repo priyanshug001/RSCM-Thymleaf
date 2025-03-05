c970d86360574cf6705f0cfe9a428056f2b4b3f6
priyanshug001 <154699980+priyanshug001@users.noreply.github.com>
2025-03-05 16:41:12 +0530

    Update MainController.java

--- src/main/java/com/RSCM/Controller/MainController.java
+++ src/main/java/com/RSCM/Controller/MainController.java
@@ -3,6 +3,7 @@ package com.RSCM.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;

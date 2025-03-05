699436eb623556f0aeb68a44fce654988762a208
priyanshug001 <154699980+priyanshug001@users.noreply.github.com>
2025-03-05 13:54:07 +0530

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/RSCM/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error"; // Must match `error.html` in the templates folder
+    }
+}

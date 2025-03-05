089244cbadf93f83cd2d6bc3c8f40574860773bb
priyanshug001 <154699980+priyanshug001@users.noreply.github.com>
2025-03-05 16:29:05 +0530

    Update RscmThymleafApplication.java

--- src/main/java/com/RSCM/RSCM_Thymleaf/RscmThymleafApplication.java
+++ src/main/java/com/RSCM/RSCM_Thymleaf/RscmThymleafApplication.java
@@ -3,7 +3,7 @@ package com.RSCM.RSCM_Thymleaf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.RSCM.RSCM_Thymleaf")
+@SpringBootApplication
 public class RscmThymleafApplication {
 
 	public static void main(String[] args) {
